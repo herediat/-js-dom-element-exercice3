ef67be7edf38bb7117b37a167d8f5b7e7f25ab61
herediat <ts.heredia@outlook.com>
2021-11-22 03:29:18 +0100

    saved

---
+++ dom_recuperation_elements_part2_exercice4/main.js
@@ -0,0 +1,53 @@
+
+// //exo1
+// let redPurple = document.getElementsByClassName(".redPurple")
+// console.log(redPurple);
+
+
+
+// //exo2
+// let redPurple1 = document.querySelectorAll(".redPurple")
+// console.log(redPurple1);
+
+// //exo3
+// let redPurple2 = document.querySelectorAll(".redPurple")[0]
+// console.log(redPurple2);
+
+// //exo4
+// let allelements = document.querySelectorAll("li, p, span")
+// console.log(allelements);
+
+
+// //exo5
+// let allelements2 = document.querySelectorAll(".important, p")
+// console.log(allelements2);
+
+
+// //exo6
+// let allelements3 = document.querySelectorAll(".redPurple")
+// console.log(allelements3);
+
+//###############################################################
+
+//1 
+let redPurple = document.getElementsByClassName('redPurple')
+console.log(redPurple);
+//2
+let redPurple2 = document.querySelectorAll('.redPurple')
+console.log(redPurple2);
+
+//3
+let recupH1 = document.querySelector('h1.redPurple')
+console.log(recupH1);
+
+//4
+let exo4 = document.querySelectorAll('li,p,span')
+console.log(exo4);
+
+//5
+let exo5 = document.querySelectorAll('li.important,p')
+console.log(exo5);
+
+//6
+let exo6 = document.querySelectorAll('h1.redPurple,span.redPurple')
+console.log(exo6);
